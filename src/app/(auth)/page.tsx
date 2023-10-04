b261d3aec855fd0ffd800044096ad244dfc5ad37
BillyJean04 <bozhozda@gmail.com>
2023-10-04 08:50:51 +0300

    feat: auth

---
+++ src/app/(auth)/page.tsx
@@ -0,0 +1,7 @@
+import Login from "@/components/auth/login";
+
+const AuthPage = () => {
+    return <Login />;
+};
+
+export default AuthPage;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Open_Sans } from "next/font/google";
+import { SupabaseProvider } from "@/lib/supabase";
 
-const inter = Inter({ subsets: ["latin"] });
+const openSans = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
     title: "Task Flow",
@@ -11,8 +12,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en">
-            <body className={inter.className}>{children}</body>
-        </html>
+        <SupabaseProvider>
+            <html lang="en">
+                <body className={openSans.className}>{children}</body>
+            </html>
+        </SupabaseProvider>
     );
 }
